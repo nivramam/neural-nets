b458282f51cf192643807363d9cc29db7bd841d0
Niveydhithaa R <36819505+nivramam@users.noreply.github.com>
2020-03-30 09:54:46 +0530

    Create normalNN.py

---
+++ normalNN.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class Neuron:
+    def __init__(self, weights,bias):
+        self.weights = weights
+        self.bias = bias
+    def feed_forward(self,input):
+        total = np.dot(self.weights,input)+self.bias
+        return sigmoid(total)
+
+weights = np.array([0,1])
+bias =4
+n = Neuron(weights,bias)
+x = np.array([2,3])
+
+print(n.feed_forward(x))
